736547f7f3d1c023bc543c55102cb930e4fee7f5
richardjtelford <richard.telford@bio.uib.no>
2020-10-09 19:18:15 +0200

    generalise startup message

--- R/zzz.R
+++ R/zzz.R
@@ -6,7 +6,7 @@
   if(rstudioapi::versionInfo()$version < 1.3){
     warning("You have an old version of Rstudio which may not completely support learnr tutorial.\nPlease update if possible")
   } else {
-    packageStartupMessage("Available tutorials shold show up in the Tutorial tab.\nNormally this is in the top right pane next to the Environment and History tabs\nAlternatively, find available packages with\nlearnr::available_tutorials(package = \"data.handling\") \nand launch them with \nlearnr::run_tutorial(..., package = \"data.handling\")\nwhere ... is the name of the tutorial")
+    packageStartupMessage("Available tutorials shold show up in the Tutorial tab.\nNormally this is in the top right pane next to the Environment and History tabs\nAlternatively, find available packages with\nlearnr::available_tutorials(package = \"", packageName(), "\") \nand launch them with \nlearnr::run_tutorial(..., package = \"", packageName(), "\")\nwhere ... is the name of the tutorial")
     
   }
 }
