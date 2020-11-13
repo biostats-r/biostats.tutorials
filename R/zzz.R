d6f501f422a6e5d613edf0560862ed11a8295788
richardjtelford <richard.telford@bio.uib.no>
2020-11-13 11:00:33 +0100

    improved onAttach

--- R/zzz.R
+++ R/zzz.R
@@ -4,7 +4,7 @@
     return()
   }
   if(rstudioapi::versionInfo()$version < 1.3){
-    warning("You have an old version of Rstudio which may not completely support learnr tutorial.\nPlease update if possible")
+    warning("You have an old version of RStudio which may not completely support learnr tutorial.\nPlease upgrade RStudio if possible")
   } else {
     packageStartupMessage("Available tutorials shold show up in the Tutorial tab.\nNormally this is in the top right pane next to the Environment and History tabs\nAlternatively, find available packages with\nlearnr::available_tutorials(package = \"", packageName(), "\") \nand launch them with \nlearnr::run_tutorial(..., package = \"", packageName(), "\")\nwhere ... is the name of the tutorial")
     
