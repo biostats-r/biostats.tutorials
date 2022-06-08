c571ef63d88c58d29e5349313eb2a8d5fe70b343
richardjtelford <richard.telford@bio.uib.no>
2022-06-08 11:39:27 +0200

    spaces round packages name

--- R/zzz.R
+++ R/zzz.R
@@ -8,7 +8,7 @@
     warning("You have an old version of RStudio which may not completely support learnr tutorial.\nPlease upgrade RStudio if possible")
   } else {
     packageStartupMessage(
-      "Welcome to the", packageName(), "package.\n
+      "Welcome to the ", packageName(), " package.\n
 Available tutorials shold show up in the Tutorial tab.
 Normally this is in the top right pane next
 to the Environment and History tabs\n
