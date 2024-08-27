bdb3bedd2d9e81d105dda80cbb3e5e976dccbb0c
RichardJTelford <Richard.Telford@uib.no>
2024-08-27 16:30:58 +0200

    remove unneeded package, bump version, quote ...

--- R/zzz.R
+++ R/zzz.R
@@ -13,7 +13,7 @@ To see available apps, run ?biostats_apps
 Alternatively, find available packages with\n
       learnr::available_tutorials(package = \"", packageName(), "\") \n
 and launch them with \n
-      learnr::run_tutorial(..., package = \"", packageName(), "\")\n
+      learnr::run_tutorial(\"...\", package = \"", packageName(), "\")\n
 where ... is the name of the tutorial"
   )
 }
